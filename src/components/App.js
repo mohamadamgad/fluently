6b31e35e263d00b9cba2be02330a458b091ae7a9
mohamadamgad <m.amgad@tubics.com>
2020-05-17 00:33:21 +0200

    Feat: Add test for the home page/ create a components folder

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h2>Welcome to Fluently</h2>
+                <p>Choose your language</p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
