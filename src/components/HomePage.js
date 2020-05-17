ae829e063f39e2d099bca9923e3e2915dfb85918
mohamadamgad <mohamg70@gmail.com>
2020-05-17 20:31:57 +0200

    refactor: remove unused code

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,7 +7,6 @@ function HomePage() {
     return (
         <div>
             <h2>Welcome to Fluently</h2>
-            <p>Choose your language</p>
             <Link to="test">
                 <Button variant="contained" color="primary">
                     Start Test
