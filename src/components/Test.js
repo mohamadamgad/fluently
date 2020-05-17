4f63e4e2461564c8426ccff7692032fedf193345
mohamadamgad <mohamg70@gmail.com>
2020-05-17 23:34:14 +0200

    feat: add styles to the questions/answers

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,7 +1,41 @@
 import React from "react";
 
+const container = {
+    width: "50%"
+};
+
+const answerContainer = {
+    display: "grid",
+    gridTemplateAreas: '"span span" "span span"'
+};
+const answerText = {
+    border: "1px solid",
+    borderRadius: "15px"
+};
+const answerParagraph = {
+    margin: 0
+};
+
 function Test() {
-    return <h1>Test</h1>;
+    return (
+        <div style={container}>
+            <h3>There isn't ....... sugar in this coffee!</h3>
+            <div style={answerContainer}>
+                <span style={answerText}>
+                    <p style={answerParagraph}>over</p>
+                </span>
+                <span style={answerText}>
+                    <p style={answerParagraph}>no</p>
+                </span>
+                <span style={answerText}>
+                    <p style={answerParagraph}>any</p>
+                </span>
+                <span style={answerText}>
+                    <p style={answerParagraph}>some</p>
+                </span>
+            </div>
+        </div>
+    );
 }
 
 export default Test;
