9a412260195560a17f865c91577ec1133ecc1de6
mohamadamgad <mohamg70@gmail.com>
2020-05-20 01:55:39 +0200

    feat: green highligh the correct selected answer

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const container = {
     width: "50%"
@@ -22,32 +22,46 @@ const answerParagraph = {
     padding: "10px"
 };
 
-function selectAnswer(answer) {
-    console.log("select answer", answer);
-}
-
 const data = {
     question: "There isn't ....... sugar in this coffee!",
     answers: [
-        { id: 1, text: "over", check: 0 },
-        { id: 2, text: "no", check: 0 },
-        { id: 3, text: "any", check: 1 },
-        { id: 4, text: "some", check: 0 }
+        { id: 0, text: "over", check: 0 },
+        { id: 1, text: "no", check: 0 },
+        { id: 2, text: "any", check: 1 },
+        { id: 3, text: "some", check: 0 }
     ]
 };
 
 function Test() {
+    const [answerTextStyle, setanswerTextStyle] = useState(answerText);
+    function selectAnswer(answer) {
+        if (answer.check === 1) {
+            console.log("select answer before", answerTextStyle);
+            setanswerTextStyle({
+                border: "1px solid green",
+                borderRadius: "25px",
+                justifyItems: "center",
+                alignItems: "center",
+                cursor: "pointer"
+            });
+            console.log("select answer after", answerTextStyle);
+        }
+    }
     return (
         <div style={container}>
             <h2>Question 1/20</h2>
             <h3>{data.question}</h3>
             <div style={answerContainer}>
-                {data.answers.map(answer => {
+                {data.answers.map((answer, index) => {
                     return (
                         <span
                             key={answer.id}
-                            onClick={() => selectAnswer(answer.check)}
-                            style={answerText}
+                            onClick={() => selectAnswer(answer)}
+                            style={
+                                answer.check === 1
+                                    ? answerTextStyle
+                                    : answerText
+                            }
                         >
                             <p style={answerParagraph}>{answer.text}</p>
                         </span>
