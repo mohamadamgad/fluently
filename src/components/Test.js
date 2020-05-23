8bb74cfa9d34a2fc3ba0a7d3314b429c1ea8ab47
mohamadamgad <mohamg70@gmail.com>
2020-05-23 17:21:32 +0200

    feat: show red border on selecting wrong answer

--- src/components/Test.js
+++ src/components/Test.js
@@ -18,12 +18,28 @@ const answerText = {
     alignItems: "center",
     cursor: "pointer"
 };
+const correctAnswer = {
+    border: "1px solid #2cbe4e",
+    borderRadius: "25px",
+    justifyItems: "center",
+    alignItems: "center",
+    cursor: "pointer"
+};
+
+const wrongAnswer = {
+    border: "1px solid red",
+    borderRadius: "25px",
+    justifyItems: "center",
+    alignItems: "center",
+    cursor: "pointer"
+};
 const answerParagraph = {
     margin: 0,
     padding: "10px"
 };
 
 function Test() {
+    const [myStyle, setmyStyle] = useState({ active: null });
     const [data, setData] = useState([]);
     let [questionNumber, setQuestionNumber] = useState(0);
     const [answerTextStyle, setanswerTextStyle] = useState(answerText);
@@ -52,22 +68,34 @@ function Test() {
             setanswerTextStyle(answerText);
         }, 1000);
     }
+
+    function updateActiveDiv(value) {
+        console.log("value", value);
+        if (value === myStyle.active) {
+            value = null;
+        }
+        setmyStyle({ active: value });
+    }
+
     return (
         <div style={container}>
             <h2>Question {questionNumber + 1}/20</h2>
             <h3>{data[questionNumber] && data[questionNumber].question}</h3>
             <div style={answerContainer}>
                 {data[questionNumber] &&
-                    data[questionNumber].answers.map(answer => {
+                    data[questionNumber].answers.map((answer, index) => {
+                        let color =
+                            myStyle.active === index && answer.check === 1
+                                ? correctAnswer
+                                : myStyle.active === index && answer.check === 0
+                                ? wrongAnswer
+                                : answerText;
                         return (
                             <span
                                 key={answer.id}
-                                onClick={() => selectAnswer(answer)}
-                                style={
-                                    answer.check === 1
-                                        ? answerTextStyle
-                                        : answerText
-                                }
+                                onClick={() => updateActiveDiv(index)}
+                                // onClick={() => selectAnswer(answer)}
+                                style={color}
                             >
                                 <p style={answerParagraph}>{answer.text}</p>
                             </span>
