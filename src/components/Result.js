e9bfd2e84e4e84e55862ababefa81aa6ffe2c7d6
mohamadamgad <mohamg70@gmail.com>
2020-05-27 23:40:03 +0200

    feat: create the result page component

---
+++ src/components/Result.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Result() {
+    return (
+        <div>
+            <h3>Thanks for fnishing the test</h3>
+            <p>your result is</p>
+        </div>
+    );
+}
+
+export default Result;
