4662a1ee3d077c8c667156110aca46db67748d9e
mohamadamgad <mohamg70@gmail.com>
2020-06-04 23:45:14 +0200

    refactor: change color of the score to green

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,13 +1,20 @@
 import React from "react";
 
+const resultNumber = {
+    color: "#2cbe4e",
+    fontWeight: "bold"
+};
+
 function Result(props) {
     return (
         <div>
             <h3>Thanks for finishing the test</h3>
             <p>
                 You are &nbsp;
-                <b>{(props.location.state._score / 20) * 100} %</b> &nbsp;
-                Fluent in {props.location.state.language}
+                <span style={resultNumber}>
+                    {Math.floor((props.location.state._score / 20) * 100)} %
+                </span>{" "}
+                &nbsp; Fluent in {props.location.state.language}
             </p>
         </div>
     );
