e5740e11dea0fa90286e17f727c16d625f6bc0e6
mohamadamgad <mohamg70@gmail.com>
2020-06-04 23:57:28 +0200

    feat: give the presented number a color based on the score

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,18 +1,24 @@
 import React from "react";
 
-const resultNumber = {
+const goodResult = {
     color: "#2cbe4e",
     fontWeight: "bold"
 };
 
+const poorResult = {
+    color: "red",
+    fontWeight: "bold"
+};
+
 function Result(props) {
+    const score = Math.floor((props.location.state._score / 20) * 100);
     return (
         <div>
             <h3>Thanks for finishing the test</h3>
             <p>
                 You are &nbsp;
-                <span style={resultNumber}>
-                    {Math.floor((props.location.state._score / 20) * 100)} %
+                <span style={score >= 50 ? goodResult : poorResult}>
+                    {score} %
                 </span>{" "}
                 &nbsp; Fluent in {props.location.state.language}
             </p>
