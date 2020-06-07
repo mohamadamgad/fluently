3fd35bcd3f7e27fee972750a81281c3eed0da1f0
mohamadamgad <mohamg70@gmail.com>
2020-06-07 22:08:19 +0200

    feat: add link to go to the homepage in the result page

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
+import { useHistory } from "react-router-dom";
 
 const goodResult = {
     color: "#2cbe4e",
@@ -12,6 +14,13 @@ const poorResult = {
 
 function Result(props) {
     const score = Math.floor((props.location.state._score / 20) * 100);
+    const history = useHistory();
+
+    function handleClick() {
+        history.push({
+            pathname: "/"
+        });
+    }
     return (
         <div>
             <h4>You are</h4>
@@ -21,6 +30,9 @@ function Result(props) {
                 </span>{" "}
             </h2>
             <h2>Fluent in {props.location.state.language}</h2>
+            <Button variant="contained" onClick={handleClick} color="primary">
+                Home Page
+            </Button>
         </div>
     );
 }
