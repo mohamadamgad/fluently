5eae98dadeded4554baa8b307e8145a83bc9ac66
mohamadamgad <mohamg70@gmail.com>
2020-09-27 13:07:04 +0200

    add the dbjson file

--- src/actions/testActions.js
+++ src/actions/testActions.js
@@ -1,9 +1,11 @@
 import dispatcher from "../appDispatcher";
 import * as testApi from "../api/dataApi";
 import actionTypes from "./actionTypes";
+import db from "../api/db.json";
 
 export async function loadTest(id) {
-    const testData = await testApi.getTest(id);
+    // const testData = await testApi.getTest(id);
+    const testData = db[id];
     console.log("testData actioins", testData);
     dispatcher.dispatch({
         actionType: actionTypes.LOAD_TEST,
